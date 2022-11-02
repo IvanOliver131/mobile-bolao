813ea67406f063c0ba0317da0ea6bbfcfff95c3b
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-11-01 23:36:35 -0300

    feat: themes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import { Center, NativeBaseProvider, Text } from "native-base";
+
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24}>
+          Hello world!
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
