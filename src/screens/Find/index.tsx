40fca9933f641892b07dbc2e00eba0beac368e1f
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-11-21 17:56:27 -0300

    feat: updating the screen of pools

--- src/screens/Find/index.tsx
+++ src/screens/Find/index.tsx
@@ -1,10 +1,65 @@
-import { Heading, VStack } from "native-base";
+import { Heading, useToast, VStack } from "native-base";
 
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
 
 export function Find() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = useState("");
+  const toast = useToast();
+  const { navigate } = useNavigation();
+
+  async function handleJoinPool() {
+    try {
+      setIsLoading(true);
+
+      if (!code.trim()) {
+        return toast.show({
+          title: "Iforme o código.",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+
+      await api.post("/pools/join", { code });
+
+      return toast.show({
+        title: "Você entrou no bolão com sucesso!",
+        placement: "top",
+        bgColor: "red.500",
+      });
+
+      navigate("pools");
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      if (error.response?.data?.message === "Pool not found") {
+        return toast.show({
+          title: "Bolão não encontrado.",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+
+      if (error.response?.data?.message === "You already joined this pool.") {
+        return toast.show({
+          title: "Você já está nesse bolão.",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+
+      return toast.show({
+        title: "Não foi possivel encontrar o bolão.",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Buscar por código" showBackButton />
@@ -20,9 +75,19 @@ export function Find() {
           Encontrar um bolão através de {"\n"} seu código único!
         </Heading>
 
-        <Input mb={2} placeholder="Qual código do bolão?" />
+        <Input
+          mb={2}
+          placeholder="Qual código do bolão?"
+          autoCapitalize="characters"
+          onChangeText={setCode}
+          value={code}
+        />
 
-        <Button title="Buscar Bolão" />
+        <Button
+          title="Buscar Bolão"
+          isLoading={isLoading}
+          onPress={handleJoinPool}
+        />
       </VStack>
     </VStack>
   );
--- src/screens/Pools/index.tsx
+++ src/screens/Pools/index.tsx
@@ -1,13 +1,46 @@
-import { VStack, Icon } from "native-base";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 
 import { Octicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { api } from "../../services/api";
+import { useCallback, useState } from "react";
+import { PoolCard, PoolCardProps } from "../../components/PoolCard";
+import { Loading } from "../../components/Loading";
+import { EmptyPoolList } from "../../components/EmptyPoolList";
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Não foi possivel carregar os bolões",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  // Sempre que colocar o foco aqui irá recarregar
+  useFocusEffect(
+    useCallback(() => {
+      fetchPools();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -29,6 +62,19 @@ export function Pools() {
           onPress={() => navigate("find")}
         ></Button>
       </VStack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />
+      )}
     </VStack>
   );
 }
